7325a731014d42c44fcf26988102ec8709185053
Norberto Oliveira Junior <norberto.oliveira@wiremaze.com>
2015-06-01 10:30:16 +0100

    Create Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BolsaDeEmpregos::Application.config.secret_key_base = 'b5f05b811ed09a391909b0e70ef4730cc8e753252f5bbcc656d2973a2db688c89a29adab9811ec21c4e4f90648f33ef1b10ebe3873a9c1454a54e61c4f6f9d38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BolsaDeEmpregos::Application.config.session_store :cookie_store, key: '_bolsa_de_empregos_session'
