86158a0aa95b59080cfe923dbebe8acb5325b71f
Norberto Oliveira Junior <norberto.oliveira@wiremaze.com>
2015-06-02 11:31:22 +0100

    Add an Entity on seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Entity.create({
+  email: 'geral@wiremaze.com',
+  password: '123456',
+  password_confirmation: '123456',
+  name: 'Wiremaze',
+  address: 'Edifício Capitólio - Torre Venetto Av. França 256, Piso 6, Sala 6.8',
+  zipcode: '4050-476',
+  place: 'Porto',
+  site: 'http://wiremaze.com',
+  nif: '1234567',
+  professional_activity: 'Software Development',
+  presentation: 'Criamos soluções que ajudam os nossos clientes a
+                 ultrapassar os seus desafios de forma simples.',
+  contact: '(+351) 228 328 813',
+  other_contact: '(+351) 228 328 823'
+})
