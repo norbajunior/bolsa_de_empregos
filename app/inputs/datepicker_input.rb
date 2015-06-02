9683ae4dce1a9795b7731071e73924028f81c795
Norberto Oliveira Junior <norberto.oliveira@wiremaze.com>
2015-06-02 17:37:40 +0100

    Add datepicker js plugin and a custom simple form input

---
+++ app/inputs/datepicker_input.rb
@@ -0,0 +1,12 @@
+class DatepickerInput < SimpleForm::Inputs::StringInput
+  def input_html_options
+    value = object.send(attribute_name)
+
+    options = {
+      value: value.nil?? nil : I18n.l(value),
+      data: { provide: 'datepicker', 'date-format' => 'dd/mm/yyyy' }
+    }
+
+    super.merge options
+  end
+end
