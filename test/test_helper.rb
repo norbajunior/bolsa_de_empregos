cc7ebf43d6361a2a8b86da3e8a081269113eea40
Norberto Oliveira Junior <norberto.oliveira@wiremaze.com>
2015-06-02 11:23:43 +0100

    Add 'has_secure_password' to User model
     - Added 'bcrypt-ruby' gem to generate the encrypted password
     - Added digest helper method to provide encrypted passwords
       on tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,11 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
