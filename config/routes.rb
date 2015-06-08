8dec9d4a79df4f9eaf4da4b656a73a08e6d4d9f3
Norberto Oliveira Junior <norberto.oliveira@wiremaze.com>
2015-06-08 16:07:22 +0100

    Create Home page
    This page list the last offers, entities, news and candidates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BolsaDeEmpregos::Application.routes.draw do
+  root 'dashboard#index'
+
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
