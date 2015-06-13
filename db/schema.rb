d78ac2da33875e598456f9e909d472df17ebbbd9
Norberto Oliveira Junior <norberto.oliveira@wiremaze.com>
2015-06-13 13:20:26 +0100

    remove hstore extension unused

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20150612163007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "applications", force: true do |t|
     t.integer  "offer_id"
