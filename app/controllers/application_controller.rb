b226c773000abdb72ee38245fc016e3de115d8e8
Norberto Oliveira Junior <norbejunior@gmail.com>
2015-07-07 22:43:15 +0100

    Refactor Entities
    Now the search is via ajax and the pagination as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  respond_to :html, :json
+  respond_to :html, :json, :js
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: I18n.t('flash.forbidden')
