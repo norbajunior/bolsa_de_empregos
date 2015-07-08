fffd05cf67cdf653ac183791c8e2185f3081849e
Norberto Oliveira Junior <norbejunior@gmail.com>
2015-07-09 00:30:41 +0100

    Add a message for when return none results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def normalize_html_text(html, length: 170)
     raw(strip_tags(html.html_safe)[0..length] + ' ...')
   end
+
+  def search_results_message
+    content_tag(:div, I18n.t('flash.none_results_found'), class: 'alert alert-info')
+  end
 end
